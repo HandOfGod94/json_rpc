4b04e870ab349f163ce97afeb1b6da95e6bbe9c8
Gahan Rakholia <gahan94rakh@gmail.com>
2025-04-12 23:32:37 +0530

    use dynamic method defintion instead of hash map based lookup

--- lib/json_rpc/router.rb
+++ lib/json_rpc/router.rb
@@ -6,33 +6,27 @@ module JsonRpc
 
         router = Router.new
         router.instance_eval(&block)
+        router
       end
     end
 
-    attr_reader :routes
-
     def initialize
-      @routes = {}
+      @rpc_methods = Module.new
+      extend(@rpc_methods)
     end
 
     def rpc(method, &block)
-      @routes[method] = block if block_given?
+      @rpc_methods.define_method(method, &block) if block_given?
       self
     end
 
-    def has_rpc?(method)
-      @routes.key?(method)
-    end
+    def has_rpc?(method) = respond_to?(method)
 
     def invoke(request)
-      result = fetch_handler(request.method).call
+      result = send(request.method)
       JsonRpc::Response.new(id: request.id, result: result, method: request.method)
     rescue StandardError => e
       JsonRpc::Response.new(id: request.id, result: nil, error: e.message, method: request.method)
     end
-
-    private
-
-    def fetch_handler(method) = routes[method]
   end
 end
